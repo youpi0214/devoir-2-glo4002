f589d2c35ea577bef9e740ea4db6801131673cd7
Jni <jni.viens@gmail.com>
2020-09-06 21:01:03 -0400

    Fixes typo

--- src/main/java/ca/ulaval/glo4002/mockexercise/StartByTestingThis.java
+++ src/main/java/ca/ulaval/glo4002/mockexercise/StartByTestingThis.java
@@ -5,7 +5,7 @@ import ca.ulaval.glo4002.mockexercise.do_not_edit.Invoice;
 public class StartByTestingThis {
 
     public Invoice oneClickBuy(String clientEmail, String productSku) {
-        // Étape 1 : Créer le art avec le CartFactory
+        // Étape 1 : Créer le cart avec le CartFactory
         // Étape 2 : Trouver le produit avec le ProductRepository
         // Étape 3 : Ajouter le produit au cart
         // Étape 4 : Pour chaque item du cart, ajouter une ligne sur l'invoice
